df9facb64d703b5019e51d14d5bdd651770c260f
joelcuthriell <joel@cuthriell.com>
2020-11-21 17:16:38 -0600

    restarting from scratch

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
-import './App.less';
-
-import { InputNumber } from 'antd';
-
-function onChange(value) {
-  console.log('changed', value);
-}
+import './App.css';
 
 function App() {
-
-
-
   return (
     <div className="App">
-
-    <InputNumber min={1} max={10} defaultValue={3} onChange={onChange} />
-    
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
